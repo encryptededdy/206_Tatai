0a702292707c0e96a7bcdef41b190a2024a42f82
Edward Zhang <edward@zhang.nz>
2017-09-07 16:59:51 +1200

    looks like I can still get wifi on the plane lmao

--- src/tatai/app/questions/Round.java
+++ src/tatai/app/questions/Round.java
@@ -5,18 +5,27 @@ import tatai.app.questions.generators.QuestionGenerator;
 import java.util.ArrayList;
 
 /**
- * 
+ *  Represents a game Round. Stores a list of questions.
  */
 public class Round {
     private ArrayList<Question> _questions = new ArrayList<>();
     private int _currentQuestion = -1;
 
+    /**
+     * Constructs a Round
+     * @param generator The QuestionGenerator to be used to generate the questions
+     * @param numQuestions Number of questions in the round
+     */
     public Round(QuestionGenerator generator, int numQuestions) {
         for (int i = 0; i < numQuestions; i++) {
             _questions.add(new Question(generator));
         }
     }
 
+    /**
+     * Checks whether there are any questions left in the round
+     * @return boolean representing if there are any questions left
+     */
     public boolean hasNext() {
         return _questions.size() > _currentQuestion+1;
     }
@@ -31,10 +40,19 @@ public class Round {
         return currentQuestion().checkAnswer(answer);
     }
 
+    /**
+     * Returns the correct answer to the current questions
+     * Note: Do not use this to check answers. Use checkAnswer() instead.
+     * @return String for the current answer
+     */
     public String currentAnswer() {
         return currentQuestion().getAnswer();
     }
 
+    /**
+     * Move on to the next question in the round and get the next question
+     * @return String with the next question in the round
+     */
     public String next() {
         if (hasNext()) {
             _currentQuestion++;
@@ -44,6 +62,10 @@ public class Round {
         }
     }
 
+    /**
+     * Whether this is the user's last allowed attempt at the current question
+     * @return boolean representing whether this is the last question
+     */
     public boolean isLastAttempt() {
         return currentQuestion().isLastAttempt();
     }
