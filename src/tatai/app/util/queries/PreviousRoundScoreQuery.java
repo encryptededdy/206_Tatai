278d5ca8d6ac22b15dc07b88ccc5ed3a3952fbf0
Zach Huxford <zachbwh@gmail.com>
2017-09-26 13:03:13 +1300

    added labels to end of round graph and changed color for most recent round.

--- src/tatai/app/util/queries/PreviousRoundScoreQuery.java
+++ src/tatai/app/util/queries/PreviousRoundScoreQuery.java
@@ -1,8 +1,23 @@
 package tatai.app.util.queries;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import tatai.app.Main;
 
 import java.sql.ResultSet;
@@ -15,16 +30,23 @@ public class PreviousRoundScoreQuery extends Query {
     }
 
     public void execute() {
-        XYChart.Series recentScoresSeries = new XYChart.Series();
+        XYChart.Series<String, Integer> recentScoresSeries = new XYChart.Series();
 
         Task<Void> task = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
                 try {
                     ResultSet rs = Main.database.returnOp(SQLQuery);
-                    int count = rs.getInt(1);
+                    int count = 10;
                     while (rs.next()) {
-                        recentScoresSeries.getData().add(new XYChart.Data(new Integer(count).toString(), rs.getInt(2)));
+                        XYChart.Data data;
+                        if (count == 1) {
+                            data = createData(Integer.toString(count), rs.getInt(2), Color.web("0x3F51B5"));
+                        } else {
+                            data = createData(Integer.toString(count), rs.getInt(2),Color.web( "0x455864"));
+                        }
+
+                        recentScoresSeries.getData().add(data);
                         count--;
                     }
 
@@ -37,9 +59,29 @@ public class PreviousRoundScoreQuery extends Query {
         };
         task.setOnSucceeded(event -> {
             _recentRoundScoresBarChart.getData().add(recentScoresSeries);
+
             completeQuery();
         });
 
         new Thread(task).start();
+    }
+
+    private XYChart.Data createData(String axisLabel, int value, Color color) {
+        XYChart.Data data =  new XYChart.Data(axisLabel, value);
+
+        String text = Integer.toString(value);
+
+        StackPane node = new StackPane();
+        Label label = new Label(text);
+        label.setTextFill(new Color(1, 1, 1, 1));
+        Group group = new Group(label);
+        StackPane.setAlignment(group, Pos.TOP_CENTER);
+        StackPane.setMargin(group, new Insets(5, 0, 0, 0));
+
+        node.setBackground(new Background(new BackgroundFill(color, new CornerRadii(4.0), null)));
+        node.getChildren().add(group);
+        data.setNode(node);
+
+        return data;
     }
 }
