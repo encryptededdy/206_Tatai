3ab3fc23516c33c971cc914724c49510b7634daa
Edward Zhang <edward@zhang.nz>
2017-10-03 13:04:26 +1300

    WIP score calculation implemented (printed at the end of round)

--- src/tatai/app/questions/Round.java
+++ src/tatai/app/questions/Round.java
@@ -2,19 +2,25 @@ package tatai.app.questions;
 
 import tatai.app.Main;
 import tatai.app.questions.generators.QuestionGenerator;
-import tatai.app.util.Database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.util.ArrayList;
 
 /**
  *  Represents a game Round. Stores a list of questions.
+ *
+ *  @author Edward
  */
 public class Round {
     private ArrayList<Question> _questions = new ArrayList<>();
     private int _currentQuestion = -1;
     private int _roundID;
     private long _startTime;
+    private QuestionGenerator _roundQuestionGenerator;
+    private int _numQuestions;
+    private Integer _score;
 
     /**
      * Constructs a Round
@@ -22,6 +28,8 @@ public class Round {
      * @param numQuestions Number of questions in the round
      */
     public Round(QuestionGenerator generator, int numQuestions) {
+        _roundQuestionGenerator = generator;
+
         System.out.println("Starting round: "+Main.database.getNextID("roundID", "rounds"));
         _roundID = Main.database.getNextID("roundID", "rounds"); // Store ID of current round
 
@@ -33,6 +41,8 @@ public class Round {
         for (int i = 0; i < numQuestions; i++) {
             _questions.add(new Question(generator, _roundID));
         }
+
+        _numQuestions = numQuestions;
 
         // Start the clock
         _startTime = Instant.now().getEpochSecond();
@@ -57,6 +67,7 @@ public class Round {
         // Round is complete. Write data.
         Main.database.insertOp("UPDATE rounds SET isComplete = 1 WHERE roundid = "+_roundID);
         Main.database.insertOp("UPDATE rounds SET roundlength = "+(Instant.now().getEpochSecond() - _startTime)+" WHERE roundid = "+_roundID);
+        System.out.println("Calculated score: "+getScore());
     }
 
     /**
@@ -92,6 +103,31 @@ public class Round {
         }
     }
 
+    public int getScore() {
+        // If score is uncalculated, then calculate score.
+        if (_score == null) {
+            // Scoring algorithm: accuracy(%) * (20 - avgQuestionLength) * ((noQuestions + 90)/100)
+            ResultSet correctRS = Main.database.returnOp("SELECT COUNT(*) FROM questions WHERE correct = 1 AND roundID = "+_roundID);
+            ResultSet qLengthRS = Main.database.returnOp("SELECT AVG(timeToAnswer) FROM questions WHERE roundID = "+_roundID);
+            try {
+                correctRS.next();
+                qLengthRS.next();
+                double accuracy = (correctRS.getDouble(1)/_numQuestions)*100;
+                double lengthScore = 20 - qLengthRS.getDouble(1);
+
+                // Calculate the score
+                _score = (int)(accuracy * lengthScore * ((_numQuestions + 90)/100));
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return 0;
+            }
+            return _score;
+        } else {
+            return _score;
+        }
+
+    }
+
     /**
      * Whether this is the user's last allowed attempt at the current question
      * @return boolean representing whether this is the last question
@@ -100,15 +136,35 @@ public class Round {
         return currentQuestion().isLastAttempt();
     }
 
+    /**
+     * Gets the number of the current question (ie. the nth question)
+     * @return The number of the current question
+     */
     public int questionNumber() {
         return _currentQuestion+1;
     }
 
+    /**
+     * Gets the current question object
+     * @return The current question object
+     */
     private Question currentQuestion() {
         return _questions.get(_currentQuestion);
     }
 
+    /**
+     * Gets the identifier of the current round (as stored in the database)
+     * @return The current round ID
+     */
     public int getRoundID() {
         return _roundID;
+    }
+
+    /**
+     * Get the name of the generator used for this round (eg. "Tens Questions"
+     * @return The generator name
+     */
+    public String getGeneratorName() {
+        return _roundQuestionGenerator.getGeneratorName();
     }
 }
